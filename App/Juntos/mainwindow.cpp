abacdada56d1f55280c77a91d0fc50705d1b3445
Romain GABEL <romain.gabel@gmail.com>
2013-12-22 16:27:43 +0100

    Icones 2

---
+++ App/Juntos/mainwindow.cpp
@@ -0,0 +1,63 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    emit on_mAccueil_clicked();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::hideAll()
+{
+    ui->cAccueil->hide();
+    ui->cPref->hide();
+    ui->cTask->hide();
+    ui->cTicket->hide();
+}
+
+
+void MainWindow::on_mAccueil_clicked()
+{
+    this->hideAll();
+    ui->TitreBody->setText("Juntos");
+    ui->cAccueil->show();
+
+}
+
+void MainWindow::on_mTask_clicked()
+{
+    this->hideAll();
+    ui->TitreBody->setText("Task");
+    ui->cTask->show();
+}
+
+void MainWindow::on_mTicket_clicked()
+{
+    this->hideAll();
+    ui->TitreBody->setText("Ticket");
+
+    ui->cTicket->show();
+}
+
+void MainWindow::on_mPreference_clicked()
+{
+    this->hideAll();
+    ui->TitreBody->setText("Préférence");
+    ui->cPref->show();
+}
+
+void MainWindow::on_mMessages_clicked()
+{
+    this->hideAll();
+    ui->TitreBody->setText("Messages");
+
+}
---
+++ App/Juntos/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_mAccueil_clicked();
+
+    void on_mTask_clicked();
+
+    void on_mTicket_clicked();
+
+    void on_mPreference_clicked();
+
+    void on_mMessages_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    void hideAll();
+};
+
+#endif // MAINWINDOW_H
